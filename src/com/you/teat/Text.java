f757f3d671009bfa2a941b108fc3995297895c0e
dp0123 <1170334620@qq.com>
2016-07-09 13:56:25 +0800

    3
    3

--- src/com/you/teat/Text.java
+++ src/com/you/teat/Text.java
@@ -5,5 +5,6 @@ public class Text {
 	public static void main(String[] args) {
 		System.out.print("-----------");
 		System.out.print("==============");
+		System.out.print("+++++++++++++++++++++++++++");
 	}
 }
